e59aa92940358c0bf370006894dc69ae8da179f0
try-max <qijiale_1128@163.com>
2020-06-08 10:10:18 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/qi/HelloGit.java
@@ -0,0 +1,11 @@
+package com.qi;
+
+/**
+ * @author Q
+ * @create 2020-06-08-9:59
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+    }
+}
