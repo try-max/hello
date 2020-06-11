e85ae9a0937386c517c71c5d08328587338c23df
try-max <qijiale_1128@163.com>
2020-06-11 17:44:18 +0800

    分支添加

--- HelloGit/src/main/java/com/qi/HelloGit.java
+++ HelloGit/src/main/java/com/qi/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("update_2!");
         System.out.println("update_3!");
         System.out.println("创建分支");
+        System.out.println("分支tianjia");
     }
 }
